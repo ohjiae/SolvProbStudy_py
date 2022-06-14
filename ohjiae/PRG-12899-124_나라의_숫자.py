0ed36b65ca5840b0cde0b85fc1ab152f237d4c27
ohjiae <ohjiae.dev@gmail.com>
2022-06-14 21:07:15 +0900

    🔴 [#1] PRG-12899

---
+++ ohjiae/PRG-12899-124_나라의_숫자.py
@@ -0,0 +1,15 @@
+def solution(n):
+    nums = [1,2,4]
+    t = 1
+    answer = ''
+    if n < 4:
+        answer=str(nums[n-1])
+    else:
+        while n > 3:
+            n -= 1
+            res = divmod(n,3)
+            n = res[0]
+            answer = str(nums[res[1]]) + answer
+            if n <= 3:
+                answer = str(nums[res[0]-1]) + answer
+    return answer
